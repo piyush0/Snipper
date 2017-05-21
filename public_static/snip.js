e7abe7b74f73e7b3646a7dc9c82f4b57926a16d3
Piyush Ajmani <piyushajmani5@gmail.com>
2017-05-22 04:38:18 +0530

    Added ace editor
    Signed-off-by: Piyush Ajmani <piyushajmani5@gmail.com>

--- public_static/snip.js
+++ public_static/snip.js
@@ -14,14 +14,16 @@ window.onload = function () {
     btn_add_snip = document.getElementById("btn_add_snip");
     title = document.getElementById("title");
     language = document.getElementById("language");
-    code = document.getElementById("code");
+    code = ace.edit("editor");
 
     btn_add_snip.onclick = function () {
+        
+
         const snip = {
             "id":id,
             "title": title.value,
             "language": language.value,
-            "code": code.value
+            "code": code.getValue()
         };
         ipcRenderer.send('new-snip-add', JSON.stringify(snip))
     }
@@ -30,6 +32,6 @@ window.onload = function () {
 ipcRenderer.on('edit', function (event, data) {
     title.value = data.title;
     language.value = data.language;
-    code.value = data.code;
+    code.setValue(data.code) ;
     id = data.id
 });
